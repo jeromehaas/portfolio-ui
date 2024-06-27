e1f56cf6628829cc2e632c94b1781478a269953f
jeromehaas <hello@jeromehaas.ch>
2024-06-27 16:55:48 +0200

    Setup: animation for tapper elements

--- src/components/partials/tapper/tapper.jsx
+++ src/components/partials/tapper/tapper.jsx
@@ -10,8 +10,8 @@ const Tapper = ({className = '', type = '', onClick = null}) => {
 	// RENDER
 	return (
 	<div className={`${className} tapper`} onClick={onClick}>
-		<div className='tapper__background'>
-			<Icon className='tapper__icon' type={type}/>
+		<div className='tapper__background background'>
+			<Icon className='tapper__icon icon' type={type}/>
 		</div>
 	</div>
 	);
